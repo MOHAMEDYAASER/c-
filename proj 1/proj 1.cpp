a3836368150cf67c4e465ce0798bfc96cf7a74b4
Mo <mo@gmail.com>
2024-03-24 22:11:48 +0200

    first commit

---
+++ proj 1/proj 1.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int age;
+	cout << "Enter Your Age\n";
+	cin >> age;
+	cout << "welcom" << endl;
+
+	if (age <18 ) {
+		cout << "Beware" << endl;
+	}
+	else {
+		cout << "dont Beware" << endl;
+	}
+
+	cout << "see you" << endl;
+
+}
